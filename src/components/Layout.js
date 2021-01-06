a57ddf902a092945bae5e8e3e50dfef8830e5f73
incode-sd <whythedevisbored@gmail.com>
2021-01-06 23:56:47 +0530

    Changing Some format and indentation.

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,8 +3,8 @@ import Navbar from "./Navbar";
 export default function Layout({ children }) {
   return (
     <div>
-      <Navbar />
-      <div className="container">{children}</div>
+       <Navbar />
+       <div className="container">{children}</div>
     </div>
   );
 }
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -3,11 +3,11 @@ import { Link } from "react-router-dom";
 import "./navbar.css";
 export default function Navbar() {
   return (
-    <nav className="mb-16 navbar">
-      <Link to="/" className="navbar--brand">
-        {" "}
-        Hotels
-      </Link>
-    </nav>
+      <nav className="mb-16 navbar">
+        <Link to="/" className="navbar--brand">
+          {" "}
+          Hotels
+        </Link>
+      </nav>
   );
 }
